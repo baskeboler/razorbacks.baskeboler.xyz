1aca2210de2871e225b3b482c8675d5aa5a508f7
Victor Gil <baskeboler@gmail.com>
2019-06-08 04:52:41 -0300

    fixes

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,12 +1,12 @@
 const config = {
   siteTitle: "Razorbacks", // Site title.
   siteTitleShort: "Death Race", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
+  siteTitleAlt: "Death Race", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://razorbacks.baskeboler.xyz", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Razorbacks - Death Race", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   siteGATrackingID: "UA-141731303-1", // Tracking code ID for google analytics.
@@ -33,11 +33,11 @@ const config = {
     },
     {
       label: "Email",
-      url: "mailto:baskeboler@gmail.com",
+      url: "mailto:yo@baskeboler.xyz",
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2019. Material User" // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2019. baskeboler" // Copyright string for the footer of the website and RSS feed.
 };
 
 // Validate
