0d22fc4d0f161007cc53a5da3161abf6981e2048
Victor Gil <baskeboler@gmail.com>
2019-06-09 02:54:43 -0300

    user description

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -6,12 +6,12 @@ const config = {
   siteUrl: "https://razorbacks.baskeboler.xyz", // Domain of your website without pathPrefix.
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "Razorbacks - Death Race", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "kickass rock n roll band from uruguay", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   siteGATrackingID: "UA-141731303-1", // Tracking code ID for google analytics.
   disqusShortname: "razorbacks-baskeboler-xyz", // Disqus shortname.
-  postDefaultCategoryID: "Culture", // Default category for posts.
+  postDefaultCategoryID: "bullshit", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   userName: "baskeboler", // Username to display in the author segment.
@@ -19,7 +19,12 @@ const config = {
   userTwitter: "baskeboler", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Montevideo, Uruguay", // User location to display in the author segment.
   userAvatar: "https://en.gravatar.com/userimage/94650559/c3e15ffb38d32ecb456e3abf4b79321e?size=200", // User avatar to display in the author segment.
-  userDescription: "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  userDescription: `Hi there, I am Victor, people call me basket, I played drums in this band and loved every minute of it.
+  Razorbacks is no longer active and it is very unlikely that we return in the near future. I work as a software developer and in order 
+  to stay up to date with new tech trends I build sites to try out new nerdy shit. This is my first try with gatsby.js, netlify and netlify-cms 
+  which are all awesome. I will be gathering as much band related material (albums, pictures, videos) as I can and will be posting it here
+  as well as whatever else that goes through my mind. I never had a blog so this may end up being my personal blog disguised as a band page lol.
+  Anyways, thats enough text, I was feeling dirty leaving the default template user description. kthxbye.`, // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [{
       label: "GitHub",
